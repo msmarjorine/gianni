6c20f0afba2aab22f4b383890805db6cd0763c13
Admin <marjorine13@gmail.com>
2023-01-03 15:22:05 +0300

    Altered configs to make possible usage of multiple env values

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs-extra');
+const path = require('path');
+
+function getConfigurationByFile(file) {
+    const pathToConfigFile = path.resolve('config', `${file}.json`);
+
+    if (!fs.existsSync(pathToConfigFile)) {
+        console.log("No custom config file found.");
+        return {};
+    }
+
+    return fs.readJson(pathToConfigFile)
+}
 
 module.exports = defineConfig({
     projectId: 'uhawz7',
@@ -6,6 +19,9 @@ module.exports = defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
             // implement node event listeners here
+            const file = config.env.configFile || ''
+
+            return getConfigurationByFile(file)
         },
         specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
         //excludeSpecPattern: "cypress/e2e/mix/*.js",
